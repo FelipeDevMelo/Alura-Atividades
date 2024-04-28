ea77fb458ff517deb1067f2ec1814bddf89a6f64
felipe melo <luizfelipemelo001@gmail.com>
2024-04-28 11:37:44 -0300

    :sparkles: Feat: sort metodo livros preco

--- alura_books/app/medotoSort.js
+++ alura_books/app/medotoSort.js
@@ -1,1 +1,6 @@
 const btnOrdenar = document.querySelector("#btnOrdenarPorPreco");
+
+btnOrdenar.addEventListener("click", () => {
+	let livrosOrdenador = livros.sort((a, b) => a.preco - b.preco);
+	exibirOsLivrosNaTela(livrosOrdenador);
+});
