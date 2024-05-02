0c0aacd2c777ce813020d8eddbdba95f34515e80
felipe melo <luizfelipemelo001@gmail.com>
2024-05-02 13:14:08 -0300

    :construtcion: promisse

--- js-consumindo-dados-api-main/index.js
+++ js-consumindo-dados-api-main/index.js
@@ -1,2 +1,5 @@
-const consultaCep = fetch("viacep.com.br/ws/01001000/json/");
+const consultaCep = fetch("https://viacep.com.br/ws/01001000/json/")
+	.then((res) => res.json())
+	.then((res) => console.log(res.localidade))
+	.catch((err) => console.log(err));
 console.log(consultaCep);
