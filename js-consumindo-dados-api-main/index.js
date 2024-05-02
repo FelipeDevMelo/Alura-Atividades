daba0913f05b506c6a4e7cf8f7aab93b2c4eafc1
felipe melo <luizfelipemelo001@gmail.com>
2024-05-02 13:45:55 -0300

    :sparkles: feat:  promisseall

--- js-consumindo-dados-api-main/index.js
+++ js-consumindo-dados-api-main/index.js
@@ -1,6 +1,15 @@
 async function buscaEndereco() {
-	const consultaCep = await fetch("https://viacep.com.br/ws/01001000/json/");
-	const consultaCepConvertida = await consultaCep.json();
-	console.log(consultaCepConvertida);
+	try {
+		const consultaCep = await fetch(
+			"https://viacep.com.br/ws/01001000/json/"
+		);
+		const consultaCepConvertida = await consultaCep.json();
+		if (consultaCepConvertida.erro) {
+			throw Error("cep não existe");
+		}
+		console.log(consultaCepConvertida);
+	} catch (erro) {
+		console.log(erro);
+	}
 }
 buscaEndereco();
