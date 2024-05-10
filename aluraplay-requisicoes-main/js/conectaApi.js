fda737e19c74f0a47118d4223d1797187b3d2382
felipe melo <luizfelipemelo001@gmail.com>
2024-05-10 12:07:45 -0300

    :sparkles: Feat: conexao api videos

--- aluraplay-requisicoes-main/js/conectaApi.js
+++ aluraplay-requisicoes-main/js/conectaApi.js
@@ -1,7 +1,10 @@
 async function listaVideos() {
-	const conexao = await fetch("http://localhost:3000/videos");
-	const conexaoConvertida = await conexao.json();
-	console.log(conexaoConvertida);
+    const conexao = await fetch("http://localhost:3000/videos");
+    const conexaoConvertida = await conexao.json();
+
+    return conexaoConvertida;
 }
 
-listaVideos();
+export const conectaApi = {
+    listaVideos
+}
