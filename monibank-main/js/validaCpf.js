99057ce9d54f1f9582d3b12940a4856661585d2e
felipe melo <luizfelipemelo001@gmail.com>
2024-05-13 11:37:08 -0300

    :sparkles: Feat: validacao cpf primeiro digito

--- monibank-main/js/validaCpf.js
+++ monibank-main/js/validaCpf.js
@@ -2,6 +2,7 @@ export default function ehUmCpf(campo) {
 	const cpf = campo.value.replace(/\.|-/g, "");
 	console.log(cpf);
 	validaNumerosRepetidos(cpf);
+	validaPrimeiroDigito(cpf);
 	console.log(validaNumerosRepetidos(cpf));
 }
 
@@ -19,4 +20,21 @@ function validaNumerosRepetidos(cpf) {
 		"99999999999",
 	];
 	return numerosRepetidos.includes(cpf);
+}
+
+function validaPrimeiroDigito(cpf) {
+	let soma = 0;
+	let multiplicador = 10;
+
+	for (let tamanho = 0; tamanho < 9; tamanho++) {
+		soma += cpf[tamanho] * multiplicador;
+		multiplicador--;
+	}
+	soma = (soma * 10) % 11;
+
+	if (soma == 10 || soma == 11) {
+		soma = 0;
+	}
+
+	return soma != cpf[9];
 }
