dc37bc523353809e8ab1bcd6cf6a58c7f3f11f06
felipe melo <luizfelipemelo001@gmail.com>
2024-05-13 11:57:12 -0300

    :sparkles: Feat: validacao idade

--- monibank-main/js/script.js
+++ monibank-main/js/script.js
@@ -1,13 +1,16 @@
-import ehUmCpf from "./validaCpf.js";
-
-const camposDoFormulario = document.querySelectorAll("[required]");
+import ehUmCPF from "./valida-cpf.js";
+import ehMaiorDeIdade from "./valida-idade.js";
+const camposDoFormulario = document.querySelectorAll('[required]')
 
 camposDoFormulario.forEach((campo) => {
-	campo.addEventListener("blur", () => verificaCampo(campo));
-});
+    campo.addEventListener("blur", () => verificaCampo(campo));
+})
 
 function verificaCampo(campo) {
-	if (campo.name == "cpf" && campo.value.length >= 11) {
-		ehUmCpf(campo);
-	}
+    if (campo.name == "cpf" && campo.value.length >= 11) {
+        ehUmCPF(campo);
+    }
+    if (campo.name == "aniversario" && campo.value != "") {
+        ehMaiorDeIdade(campo);
+    }
 }
