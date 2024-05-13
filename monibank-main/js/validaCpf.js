9d983002fcc522d7a4c4576e2bfdf0d6f69513ba
felipe melo <luizfelipemelo001@gmail.com>
2024-05-13 11:40:00 -0300

    :sparkles: Feat: validacao cpf segundo digito

--- monibank-main/js/validaCpf.js
+++ monibank-main/js/validaCpf.js
@@ -1,9 +1,15 @@
 export default function ehUmCpf(campo) {
 	const cpf = campo.value.replace(/\.|-/g, "");
 	console.log(cpf);
-	validaNumerosRepetidos(cpf);
-	validaPrimeiroDigito(cpf);
-	console.log(validaNumerosRepetidos(cpf));
+	if (
+		validaNumerosRepetidos(cpf) ||
+		validaPrimeiroDigito(cpf) ||
+		validaSegundoDigito(cpf)
+	) {
+		console.log("Esse cpf não existe!");
+	} else {
+		console.log("Existe!");
+	}
 }
 
 function validaNumerosRepetidos(cpf) {
@@ -37,4 +43,22 @@ function validaPrimeiroDigito(cpf) {
 	}
 
 	return soma != cpf[9];
+}
+
+function validaSegundoDigito(cpf) {
+	let soma = 0;
+	let multiplicador = 11;
+
+	for (let tamanho = 0; tamanho < 10; tamanho++) {
+		soma += cpf[tamanho] * multiplicador;
+		multiplicador--;
+	}
+
+	soma = (soma * 10) % 11;
+
+	if (soma == 10 || soma == 11) {
+		soma = 0;
+	}
+
+	return soma != cpf[10];
 }
